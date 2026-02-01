24f0f627c823dbec811c6b415eeb4c1574841172
ahobb92 <ahobb92@gmail.com>
2026-02-01 11:05:49 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,4 +4,26 @@ $(document).ready(function(){
     });
   });
   
+  // Contact form validation
+$(document).ready(function () {
+    $('#contactForm').on('submit', function (e) {
+      e.preventDefault();
+  
+      let isValid = true;
+  
+      $(this).find('input, textarea').each(function () {
+        if (!this.checkValidity()) {
+          $(this).addClass('is-invalid');
+          isValid = false;
+        } else {
+          $(this).removeClass('is-invalid');
+        }
+      });
+  
+      if (isValid) {
+        alert('Thank you! Your message has been sent.');
+        this.reset();
+      }
+    });
+  });
   
